6562d2ee156d001e60afe03dc3d500cc7d0ab5f6
kesav <kesavaddala5@gmail.com>
2022-07-13 21:34:12 +0530

    new

--- server.js
+++ server.js
@@ -1,41 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const BrandName = require('./model');
+const express = require("express");
+const mongoose = require("mongoose");
+const BrandName = require("./model");
 
 const app = express();
 
-app.use(express.json())
-
+app.use(express.json());
 
 // connected mongoose and console for error
-mongoose.connect('mongodb+srv://kesavaddala:Swamy@cluster0.kvybt.mongodb.net/?retryWrites=true&w=majority').then(
-    () => console.log('DB connected...') 
-).catch(err => console.log(err))
+mongoose
+  .connect(
+    "mongodb+srv://kesavaddala:Swamy@cluster0.kvybt.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB connected..."))
+  .catch((err) => console.log(err));
 
+app.post("/addbrands", async (req, res) => {
+  const { brandname } = req.body;
+  try {
+    const newData = new BrandName({ brandname });
+    await newData.save();
+    return res.json(await BrandName.find());
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
-
-app.post('/addbrands',async (req , res) =>{
-    const {brandname} = req.body ;
-    try{
-        const newData = new BrandName({brandname});
-        await newData.save();
-        return res.json(await BrandName.find())
-
-    }
-    catch(err){
-        console.log(err.message);
-    }
-})
-
-app.get('/getbrands', async (req, res) => {
-    try{
-        const allData = await BrandName.find();
-        return res.json(allData);
-
-    }
-    catch(err){
-        console.log(err.message);
-    }
-})
-
-app.listen(4000,()=>console.log('server running...'))
+app.get("/getbrands", async (req, res) => {
+  try {
+    const allData = await BrandName.find();
+    return res.json(allData);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log("server running..."));
